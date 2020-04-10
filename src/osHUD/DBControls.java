3045c7e40f42e85800a8b470f2b87f267901b31f
ollitp <olli.paakkunainen@gmail.com>
2020-04-10 17:27:51 +0300

    Skeleton class for database controls

---
+++ src/osHUD/DBControls.java
@@ -0,0 +1,39 @@
+package osHUD;
+
+/**
+ * Creates and controls data of database
+ * @author Olli
+ * @version 10.4.2020
+ *
+ */
+public class DBControls {
+
+    /**
+     * Initializes a new database
+     */
+    public static void createDatabase() {
+        
+    }
+    
+    /**
+     * Creates a new table for poker room
+     */
+    public static void createTable() {
+        
+    }
+    
+    /**
+     * Adds a new player to table
+     */
+    public static void insertPlayer(Player player) {
+        
+    }
+    
+    /**
+     * Looks for player with given name and adds current hands to data
+     * @param playerName name of the player
+     */
+    public static void appendPlayer(String playerName) {
+        
+    }
+}
--- src/osHUD/Main.java
+++ src/osHUD/Main.java
@@ -2,6 +2,8 @@ package osHUD;
 
 public class Main {
 
+    private String pokerstarsHistoryPath = "";
+    
     public static void main(String[] args) {
 	// write your code here
     }
