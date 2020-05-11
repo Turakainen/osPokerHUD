ae2777c04081033034faa238cab9eb92f9d03cef
ollitp <olli.paakkunainen@gmail.com>
2020-05-11 15:58:54 +0300

    Refactoring

--- src/osHUD/DBControls.java
+++ src/osHUD/DBControls.java
@@ -13,39 +13,43 @@ import com.sun.jna.platform.win32.DBT;
 public class DBControls {
 
     /**
-     * Initializes a new database
+     * Creates a database connection
+     * @param db database name
+     * @return database connection
      */
-    public static void createDatabase() {
+    public static Connection connectDatabase(String db) {
         Connection c = null;
         
         try {
            Class.forName("org.sqlite.JDBC");
-           c = DriverManager.getConnection("jdbc:sqlite:osPokerHUD.db");
+           c = DriverManager.getConnection("jdbc:sqlite:" + db + ".db");
+           System.out.println("Opened database successfully");
+           return c;
         } catch ( Exception e ) {
            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
            System.exit(0);
         }
-        System.out.println("Opened database successfully");
+        return c;
     }
     
     /**
      * Creates a new table for poker room
+     * @param db database name
+     * @param table name
      */
-    public static void createTable() {
+    public static void createTable(String db, String table) {
         Connection c = null;
         Statement stmt = null;
         
         try {
-           Class.forName("org.sqlite.JDBC");
-           c = DriverManager.getConnection("jdbc:sqlite:osPokerHUD.db");
-           System.out.println("Opened database successfully");
+           c = connectDatabase(db);
            
            DatabaseMetaData dbm = c.getMetaData();
            ResultSet tables = dbm.getTables(null, null, "pokerstars", null);
            
            if (!tables.next()) {
             stmt = c.createStatement();
-            String sql = "CREATE TABLE pokerstars "
+            String sql = "CREATE TABLE " + table + " "
                     + "( name          TEXT   PRIMARY KEY   NOT NULL,"
                     + " handsplayed    INT, " 
                     + " preflopcalls   INT, "
@@ -66,20 +70,20 @@ public class DBControls {
     
     /**
      * Adds a new player to table
+     * @param db database name
+     * @param table name
      * @param player player to be added
      */
-    public static void insertPlayer(Player player) {
+    public static void insertPlayer(String db, String table, Player player) {
         Connection c = null;
         Statement stmt = null;
 
         try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:osPokerHUD.db");
+            c = connectDatabase(db);
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
             
             PreparedStatement pstmt;
-            String sql = "INSERT INTO pokerstars "
+            String sql = "INSERT INTO " + table + " "
                    + "(name, handsplayed, preflopcalls, preflopbets, preflopraises) "
                    + "VALUES (?,?,?,?,?)";
             pstmt = c.prepareStatement(sql);
@@ -109,21 +113,21 @@ public class DBControls {
     
     /**
      * Prints table contents to console
+     * @param db database name
      * @param table to print
      */
-    public static void printTable(String table) {
+    public static void printTable(String db, String table) {
         Connection c = null;
         
         try {
-           Class.forName("org.sqlite.JDBC");
-           c = DriverManager.getConnection("jdbc:sqlite:osPokerHUD.db");
+           c = connectDatabase(db);
            Statement st = c.createStatement();
            ResultSet rs = st.executeQuery("SELECT * FROM " + table);
            ResultSetMetaData rsmd = rs.getMetaData();
            int columnCount = rsmd.getColumnCount();
            
            System.out.println("============================");
-           System.out.println("Table " + table + "contents:");
+           System.out.println("Table " + table + " contents:");
            while(rs.next()) {
                for(int i = 1; i <= columnCount; i++) {
                    System.out.println(rs.getString(i) + " ");
@@ -143,14 +147,16 @@ public class DBControls {
      * @param args not in use
      */
     public static void main(String[] args) {
-        createDatabase();
-        createTable();
+        String db = "osPokerHUD";
+        String table = "pokerstars";
         Player p1 = new Player("Foo");
         Player p2 = new Player("Bar");
         Player p3 = new Player("Foobar");
-        insertPlayer(p1);
-        insertPlayer(p2);
-        insertPlayer(p3);
-        printTable("pokerstars");
+        
+        createTable(db, table);
+        insertPlayer(db, table, p1);
+        insertPlayer(db, table, p2);
+        insertPlayer(db, table, p3);
+        printTable(db, table);
     }
 }
