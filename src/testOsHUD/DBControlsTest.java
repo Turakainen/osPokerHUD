660b3a99e72e24595f7750100c2394bcc3924d97
ollitp <olli.paakkunainen@gmail.com>
2020-05-12 22:37:05 +0300

    Test stubs for DBControls

---
+++ src/testOsHUD/DBControlsTest.java
@@ -0,0 +1,44 @@
+package testOsHUD;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DBControlsTest {
+
+    @Test
+    void testConnectDatabase() {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    void testCreateTable() {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    void testInsertPlayer() {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    void testUpdatePlayer() {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    void testFindPlayer() {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    void testPrintTable() {
+        fail("Not yet implemented");
+    }
+
+}
